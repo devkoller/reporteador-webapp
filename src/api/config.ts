52428aa60bdce94231173e205e98c2cef5c89cde
Julio Mariscal <devkoller.jm@gmail.com>
2025-07-15 10:14:05 -0600

    new backend route

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,4 +2,4 @@ export const PORT = window.location.protocol === "http:" ? ":3070" : ":3070"
 
 export const API_URL = import.meta.env.DEV
 	? `http://${window.location.hostname}${PORT}`
-	: "https://api-reportes.opdhcg.com"
+	: "https://api-reportes.hcg.gob.mx"
